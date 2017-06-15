f9867bdd136f7b79154125f105a167f8c9525f4f
cc <myairia@gmail.com>
2017-06-15 12:46:44 -0700

    Add packaging support.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import alluxio
+
+
+setup(
+    name='alluxio',
+    version=alluxio.__version__,
+    description='Alluxio python client',
+    long_description='Alluxio python client based on REST API',
+    url='https://github.com/TachyonNexus/alluxio-py',
+    author='Alluxio, Inc',
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=['requests'],
+)
