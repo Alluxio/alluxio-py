68d6ba1d42d1f39a8bf0f8961e48adfd39a8c47b
Jianjian <jja725@gmail.com>
2024-02-27 12:57:26 -0800

    test

--- alluxio/alluxio_file_system.py
+++ alluxio/alluxio_file_system.py
@@ -195,7 +195,6 @@ class AlluxioFileSystem:
             )
 
         self.page_size = humanfriendly.parse_size(page_size, binary=True)
-
         self.hash_provider = ConsistentHashProvider(
             etcd_hosts=etcd_hosts,
             etcd_port=etcd_port,
