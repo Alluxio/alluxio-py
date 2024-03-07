f4cdac1bd09475595a838200e5145dc4a13f9355
Lu Qiu <luqiujob@gmail.com>
2024-03-06 17:35:24 -0800

    Add get status test

--- tests/test_metadata_docker.py
+++ tests/test_metadata_docker.py
@@ -5,7 +5,12 @@ from tests.conftest import TEST_DIR
 from tests.conftest import TEST_ROOT
 
 
-def validate_list(alluxio_client: AlluxioClient, alluxio_path, local_path):
+def validate_list_get_status(
+    alluxio_client: AlluxioClient, alluxio_path, local_path
+):
+    validate_get_status(alluxio_client, alluxio_path, local_path)
+    if not os.path.isdir(local_path):
+        return
     alluxio_res = alluxio_client.listdir(alluxio_path)
     alluxio_listing = {item.name: item for item in alluxio_res}
     local_listing = list(os.scandir(local_path))
@@ -30,13 +35,32 @@ def validate_list(alluxio_client: AlluxioClient, alluxio_path, local_path):
             assert (
                 entry.stat().st_size == alluxio_entry.length
             ), f"Size mismatch for {entry.name}: expected {entry.stat().st_size}, got {alluxio_entry.length}"
-        else:
-            validate_list(
-                alluxio_client,
-                os.path.join(alluxio_path, alluxio_entry.name),
-                os.path.join(local_path, entry.name),
-            )
+        validate_list_get_status(
+            alluxio_client,
+            os.path.join(alluxio_path, alluxio_entry.name),
+            os.path.join(local_path, entry.name),
+        )
 
 
-def test_list(alluxio_client: AlluxioClient):
-    validate_list(alluxio_client, TEST_ROOT, TEST_DIR)
+def validate_get_status(
+    alluxio_client: AlluxioClient, alluxio_path: str, local_path: str
+):
+    alluxio_status = alluxio_client.get_file_status(alluxio_path)
+    local_status = os.stat(local_path)
+    expected_type = "directory" if os.path.isdir(local_path) else "file"
+    assert (
+        expected_type == alluxio_status.type
+    ), f"Type mismatch for {alluxio_path}: expected {expected_type}, got {alluxio_status.type}"
+    local_mod_time_ms = int(local_status.st_mtime * 1000)
+    assert (
+        abs(local_mod_time_ms - alluxio_status.last_modification_time_ms)
+        <= 1000
+    ), f"Last modification time mismatch for {alluxio_path}"
+    if expected_type == "file":
+        assert (
+            local_status.st_size == alluxio_status.length
+        ), f"Size mismatch for {alluxio_path}: expected {local_status.st_size}, got {alluxio_status.length}"
+
+
+def test_list_get_status(alluxio_client: AlluxioClient):
+    validate_list_get_status(alluxio_client, TEST_ROOT, TEST_DIR)
